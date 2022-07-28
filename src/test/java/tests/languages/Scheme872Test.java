018df7d8216724171582016937479fdc20263758
ServachakM <servachak.m.u@gmail.com>
2022-07-28 13:36:29 +0300

    moved tests to correct classes

---
+++ src/test/java/tests/languages/Scheme872Test.java
@@ -0,0 +1,38 @@
+package tests.languages;
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+import runner.BaseTest;
+
+public class Scheme872Test extends BaseTest {
+
+    @Test
+    public void testUrlSchemeLanguage() {
+        final String expectedUrl = "http://www.99-bottles-of-beer.net/language-scheme-582.html";
+        final String SchemeLanguage = "Scheme (original version)";
+
+        String actualUrl =
+                openBaseURL()
+                        .clickTopListsMenu()
+                        .clickTopListRealSubmenu()
+                        .clickLanguage(SchemeLanguage)
+                        .getURL();
+
+        Assert.assertEquals(actualUrl, expectedUrl);
+    }
+
+    @Test
+    public void testHeaderTextSchemeLanguage() {
+        final String expectedHeader = "Language Scheme";
+        final String SchemeLanguage = "Scheme (original version)";
+
+        String actualHeader =
+                openBaseURL()
+                        .clickTopListsMenu()
+                        .clickTopListRealSubmenu()
+                        .clickLanguage(SchemeLanguage)
+                        .getH2MainText();
+
+        Assert.assertEquals(actualHeader, expectedHeader);
+    }
+}
--- src/test/java/tests/top_lists/TopListRealTest.java
+++ src/test/java/tests/top_lists/TopListRealTest.java
@@ -86,34 +86,4 @@ public class TopListRealTest extends BaseTest {
 
         Assert.assertNotEquals(newUrl, oldUrl);
     }
-
-    @Test
-    public void testUrlSchemeLanguage() {
-        final String expectedUrl = "http://www.99-bottles-of-beer.net/language-scheme-582.html";
-        final String SchemeLanguage = "Scheme (original version)";
-
-        String actualUrl =
-                openBaseURL()
-                        .clickTopListsMenu()
-                        .clickTopListRealSubmenu()
-                        .clickLanguage(SchemeLanguage)
-                        .getURL();
-
-        Assert.assertEquals(actualUrl, expectedUrl);
-    }
-
-    @Test
-    public void testHeaderTextSchemeLanguage() {
-        final String expectedHeader = "Language Scheme";
-        final String SchemeLanguage = "Scheme (original version)";
-
-        String actualHeader =
-                openBaseURL()
-                        .clickTopListsMenu()
-                        .clickTopListRealSubmenu()
-                        .clickLanguage(SchemeLanguage)
-                        .getH2MainText();
-
-        Assert.assertEquals(actualHeader, expectedHeader);
-    }
 }
